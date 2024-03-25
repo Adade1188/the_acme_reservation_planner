e2e0f943c4569db88af2bad0ac51f87fdba13350
Adade1188 <39663857+Adade1188@users.noreply.github.com>
2024-03-25 18:52:29 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,28 +1,3 @@
-require('dotenv').config();
-const express = require('express');
-const { createTables, seed } = require('./db');
-const {
-  createCustomer,
-  createRestaurant,
-  createReservation,
-  fetchCustomers,
-  fetchRestaurants,
-  fetchReservations,
-} = require('./db');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-
-const init = async () => {
-  try {
-    await createTables();
-    await seed();
-    console.log('Database seeded');
-  }
-};
-
-  init();
 
 
